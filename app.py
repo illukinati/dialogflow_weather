c430fb96d7285579fc89acef08b086f4729dd345
Eko Prasetyo <ekoprasetyolnh@gmail.com>
2018-08-08 15:56:24 +0900

    change celcius

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def makeYqlQuery(req):
     if city is None:
         return None
 
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "' and u='c')"
 
 
 def makeWebhookResult(data):
@@ -86,7 +86,7 @@ def makeWebhookResult(data):
     # print(json.dumps(item, indent=4))
 
     speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " C"
+             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
 
     print("Response:")
     print(speech)
